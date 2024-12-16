5fbcd321d88cecc28418cabc0984cfc62b696c32
Awashhh <dddrag734@gmail.com>
2024-12-16 20:38:19 +0300

    Start

---
+++ README.md
@@ -0,0 +1,1 @@
+# Translitarator Web Application
---
+++ script/languages.js
@@ -0,0 +1,14 @@
+const languages = [
+	{
+		no: '0',
+		name: 'Cyrilic to Latin',
+		native: 'j',
+		code: 'cr-ln',
+	},
+	{
+		no: '1',
+		name: 'Latin to Cyrilic',
+		native: 'o',
+		code: 'ln-cr',
+	},
+]
